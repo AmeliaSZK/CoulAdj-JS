67fb6b849c5f760e7d452c9bb6b05aaf8825075b
Amélia SZK <amelia.szk@protonmail.com>
2020-11-30 23:01:29 -0500

    Added legalese and documentation

--- couladj.js
+++ couladj.js
@@ -1,3 +1,20 @@
+/*  CoulAdj: Reads an image and outputs the colour adjacencies in a TSV format.
+    Copyright (C) 2020  Amélia SZK
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /* There are 6 buttons on the page:
 - Choose File
 - Restore defaults
@@ -12,7 +29,7 @@ Of those, (as far as I currently know) 3 need a JavaScript implementation:
 - Save As...
 */
 
-/**
+/** Process the source image and write the output.
  * 
  * @param {Event} evt 
  */
@@ -22,7 +39,7 @@ const okButtonClick = (evt) => {
   evt.preventDefault(); // Needed to prevent the page from reloading
 }
 
-/**
+/** Copy the output to the clipboard
  * 
  * @param {Event} evt 
  */
@@ -30,7 +47,7 @@ const copyButtonClick = (evt) => {
   console.log('Copy to Clipboard button clicked.');
 }
 
-/**
+/** Open a Save File dialog to export the output in a TSV file.
  * 
  * @param {Event} evt 
  */
