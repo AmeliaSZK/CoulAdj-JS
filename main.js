5137ec2ca7cef5d5fe035c05663a967b6e517ef4
Amélia SZK <amelia.szk@protonmail.com>
2020-12-04 14:48:23 -0500

    Another attempt with `await`

--- main.js
+++ main.js
@@ -184,20 +184,9 @@ class PixelArray {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
 
-    let height;
-    let width;
-
-    // We're following the example at:
-    // https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/createImageBitmap
-    Promise.all([
-      createImageBitmap(source)
-    ]).then(function(sprites) {
-      context.drawImage(sprites[0], 0, 0);
-      height = sprites[0].height;
-      width = sprites[0].width;
-    });
-
-    return context.getImageData(0, 0, height, width);
+    const bitmap = await createImageBitmap(source);
+    context.drawImage(bitmap, 0, 0);
+    return context.getImageData(0, 0, bitmap.height, bitmap.width);
   }
 
 
